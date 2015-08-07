b78c781cf6711de71b00643c94459190ebf6e557
James Newell <james@digitaledgeit.com.au>
2015-08-08 09:27:03 +1000

    change test ciphertext to figure out ci failure

--- test/index.js
+++ test/index.js
@@ -100,11 +100,11 @@ describe('easy-encryption', function() {
       assert.equal(
         Cipher.decrypt(
           'password',
-          '46f5fdbf46f8727036b161916ce6e788$5df93a092a5ddcc05f4fb3e529a256f1208bdd7d14599b5' +
-          '685debada50634a4426a42a994800f49affffd2d8775fdd87664e52301c9a8e0607ccba1ced57703' +
-          '24308a32d88aab9f475c2d4725d32bcbdd1a7fb53575bdf81838799100ceb9fc5f767ac6a119ad8f' +
-          'd7f95732f2a1cb8b238e84f9ffd71e015876d2bcb87fc8857$5738350049341fb124a34d085bc2aa' +
-          'a121452c0ab30a5a9af93dc913481d57a7'
+          '56b3889d36096b68775c0c12db31d119$a492fd56281e63016bfad0181a72332dde31fed4929ba74' +
+          '85c13db7ad78318432b9856c9f0a92c63393600b9cbf434c47c703996a74aabb39a1738c80e36a43' +
+          'fb048d54e861b43d12ee0002c3305217832229f653baf47efdfda18ecfc4fc2adb9c3b272b3ca043' +
+          '292831030de5eaa85d88d6f4b065563553421f1edc24adfaf$9384bd1463a812ef89b6033bbd40a4' +
+          '5b70d2776712f93cd172da2a8ddfe02c41'
         ),
         'This message is TOP secret!!!'
       );
